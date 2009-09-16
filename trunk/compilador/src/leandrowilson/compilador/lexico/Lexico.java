728461d8bc091ef051447a207f8bb41e2ed7e79b
leandrodvd <leandrodvd@2794b84e-a0bc-11de-86bd-4589e38204d3>
2009-09-16 17:53:14 +0000

    VoiLa!

---
+++ trunk/compilador/src/leandrowilson/compilador/lexico/Lexico.java
@@ -0,0 +1,17 @@
+package leandrowilson.compilador.lexico;
+
+public class Lexico {
+
+	public Lexico() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
