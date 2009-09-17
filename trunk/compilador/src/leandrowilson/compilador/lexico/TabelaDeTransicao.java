68c21d81768e4f61829644a55a1b63b4ea2cceba
wilsonfaria86 <wilsonfaria86@2794b84e-a0bc-11de-86bd-4589e38204d3>
2009-09-17 14:34:32 +0000

    =P

--- trunk/compilador/src/leandrowilson/compilador/lexico/TabelaDeTransicao.java
+++ trunk/compilador/src/leandrowilson/compilador/lexico/TabelaDeTransicao.java
@@ -2,10 +2,10 @@ package leandrowilson.compilador.lexico;
 
 public class TabelaDeTransicao {
 	private int[][] tabelaDeTransicao;
-	//Estado final de erro
+	//Estado final de erro  - alterar posteriormente
 	private final int ESTADO_ERRO =9999; 
 	private final int ASCII_TAMANHO = 256;
-    private final int ESTADOS_QUANT = 20;
+    private final int ESTADOS_QUANT = 12;
     private int[] estadosFinais;
     
     
@@ -13,14 +13,20 @@ public class TabelaDeTransicao {
 		tabelaDeTransicao = new int[ESTADOS_QUANT][ASCII_TAMANHO];
 		inicializaTabelaDeTransicaoVazia();
 		inicializaEstadosFinais();
+		inicializaTabelaDeTransicao();
 	}
 
 	private void inicializaEstadosFinais() {
-		// TODO Auto-generated method stub
+		estadosFinais = new int[ESTADOS_QUANT];
+		estadosFinais[0] = 0;
+		estadosFinais[1] = 8;
+		estadosFinais[2] = 10;
+		estadosFinais[3] = 11;
 		
 	}
 
 	private void inicializaTabelaDeTransicaoVazia() {
+		//preenche toda a Tabela de Tranição com Estado de Erro
 		for (int i = 0;i<ESTADOS_QUANT;i++){
 			for (int j = 0;i<ASCII_TAMANHO;i++){
 				tabelaDeTransicao[i][j]=ESTADO_ERRO;
@@ -30,6 +36,12 @@ public class TabelaDeTransicao {
 	}
 	
 	public int proximoEstado(int estadoAtual,int caracterLido){
+		//Retorna o devido elemento da tabela de transição
 		return tabelaDeTransicao[estadoAtual][caracterLido];
+	}
+	
+	private void inicializaTabelaDeTransicao() {
+		//Preenche a Tabela de Transição
+		//tabelaDeTransicao[0][0] = 0;
 	}
 }
